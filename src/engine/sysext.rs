ba803cc5b36db12027ce012a0e148e7f246ce4b3
Ayana Yaegashi <ayaegashi@microsoft.com>
2025-08-19 23:14:33 +0000

    list

--- src/engine/sysext.rs
+++ src/engine/sysext.rs
@@ -1,10 +1,20 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, process::Command};
 
 use anyhow::{Context, Error};
 use log::debug;
+use serde::{Deserialize, Serialize};
 
 use osutils::exe::RunAndCheck;
 use trident_api::config::HostConfiguration;
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
+struct Extension {
+    name: String,
+    #[serde(rename = "type")]
+    ext_type: String,
+    path: String,
+    time: u64,
+}
 
 pub fn install_sysexts(host_config: &HostConfiguration) -> Result<(), Error> {
     // Sysexts may be stored in /etc/extensions, /run/extensions, and /var/lib/extensions All
@@ -12,6 +22,14 @@ pub fn install_sysexts(host_config: &HostConfiguration) -> Result<(), Error> {
     // contents in /usr/lib/extension-release.d/
 
     // Discover existing sysexts
+    let output_json = Command::new("systemd-sysext")
+        .arg("list")
+        .arg("--json=pretty")
+        .output_and_check()
+        .context("Failed to run `systemd-sysext list --json=pretty")?;
+    let parsed: Vec<Extension> =
+        serde_json::from_str(&output_json).context("Failed to parse systemd-sysext list output")?;
+    debug!("Found existing extensions: {:?}", parsed);
 
     // Merge new sysexts
     let sysexts = &host_config.sysexts;
@@ -40,7 +58,7 @@ pub fn install_sysexts(host_config: &HostConfiguration) -> Result<(), Error> {
         );
 
         // Call systemd-sysext
-        std::process::Command::new("systemd-sysext")
+        Command::new("systemd-sysext")
             .arg("merge")
             .run_and_check()
             .context("Failed to run `systemd-sysext merge`")?;
