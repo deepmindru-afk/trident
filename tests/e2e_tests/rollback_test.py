f8101d274216570b4b6ff450e2ce9e253e0a332a
Brian Fjeldstad <bfjelds@microsoft.com>
2025-10-24 17:03:03 +0000

    fix tests

--- tests/e2e_tests/rollback_test.py
+++ tests/e2e_tests/rollback_test.py
@@ -1,5 +1,6 @@
 import fabric
 import pytest
+import yaml
 
 from base_test import get_host_status
 
@@ -11,11 +12,35 @@ def test_rollback(
     tridentCommand: str,
     abActiveVolume: str,
 ) -> None:
+    print("Starting rollback test...")
     # Check Host Status
     host_status = get_host_status(connection, tridentCommand)
     # Assert that the servicing state is provisioned
     assert host_status["servicingState"] == "provisioned"
     # Assert that the last error reflects health.checks failure
-    assert "Failed health-checks" in host_status["lastError"]
+    serializedLastError = yaml.dump(host_status["lastError"], default_flow_style=False)
+    assert "Failed health-checks" in serializedLastError
     # Assert that the active volume has not changed
     assert host_status["abActiveVolume"] == abActiveVolume
+
+    # Check log files for expected failure messages
+    listLogsResult = connection.run(
+        "sudo ls /var/lib/trident/trident-update-check-failure-*.log"
+    )
+    # There should be 1 log file
+    assert len(listLogsResult.stdout.strip().splitlines()) == 1
+    # Get log file contents
+    logResultContentResult = connection.run(f"sudo cat {listLogsResult.stdout.strip()}")
+    # Verify that script failure message is in log file
+    assert (
+        "Script 'invoke-rollback-from-script' failed" in logResultContentResult.stdout
+    )
+    # Verify that systemd 2 failure messages are in log file
+    assert (
+        "Unit non-existent-service1.service could not be found"
+        in logResultContentResult.stdout
+    )
+    assert (
+        "Unit non-existent-service2.service could not be found"
+        in logResultContentResult.stdout
+    )
