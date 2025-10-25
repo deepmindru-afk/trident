d9c9dee9b6948e07be1f5685ff85d67e286d2c3b
Brian Fjeldstad <bfjelds@microsoft.com>
2025-10-25 03:10:51 +0000

    test diff for install and abupdate

--- tests/e2e_tests/uefi_fallback_test.py
+++ tests/e2e_tests/uefi_fallback_test.py
@@ -16,8 +16,22 @@ def test_uefi_fallback(
     host_status = get_host_status(connection, tridentCommand)
     # Assert that the servicing state is provisioned
     assert host_status["servicingState"] == "provisioned"
-    # Assert that the last error reflects health.checks failure
-    serializedLastError = yaml.dump(host_status["lastError"], default_flow_style=False)
-    assert "A/B update failed as host booted from" in serializedLastError
+
+    activeVolume = abActiveVolume
+    scenario = "clean-install"
+    if activeVolume.startswith("abupdate-"):
+        # Strip the abupdate- prefix to get the actual volume name
+        activeVolume = activeVolume[len("abupdate-") :]
+        scenario = "abupdate"
+
+    if scenario == "abupdate":
+        # Assert that the last error reflects health.checks failure
+        serializedLastError = yaml.dump(
+            host_status["lastError"], default_flow_style=False
+        )
+        assert "A/B update failed as host booted from" in serializedLastError
+    else:
+        assert host_status["lastError"] is None
+
     # Assert that the active volume has not changed
-    assert host_status["abActiveVolume"] == abActiveVolume
+    assert host_status["abActiveVolume"] == activeVolume
