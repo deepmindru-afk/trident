893951726adaf57374c3d9b5f4d0ba8f58c4b952
Brian Fjeldstad <bfjelds@microsoft.com>
2025-10-28 18:31:05 +0000

    add arg

--- tools/cmd/netlaunch/main.go
+++ tools/cmd/netlaunch/main.go
@@ -404,6 +404,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&traceFile, "trace-file", "m", "", "File for writing metrics collected from Trident.")
 	rootCmd.PersistentFlags().StringVarP(&backgroundLogstreamFull, "full-logstream", "b", "logstream-full.log", "File to write full logstream output to. (Requires -l)")
 	rootCmd.PersistentFlags().BoolVarP(&waitForProvisioned, "wait-for-provisioned-state", "", false, "Wait for Host Status servicingState to be 'provisioned'")
+	rootCmd.PersistentFlags().BoolVarP(&onlyPrintExitCode, "only-print-exit-code", "", false, "Only print the exit code")
 	rootCmd.PersistentFlags().BoolVarP(&forceColor, "force-color", "", false, "Force colored output")
 	rootCmd.PersistentFlags().BoolVarP(&secureBoot, "secure-boot", "", false, "Enable SecureBoot")
 	rootCmd.PersistentFlags().StringVarP(&signingCert, "signing-cert", "", "", "Path to signing certificate")
